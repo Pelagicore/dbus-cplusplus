e66f5fe4ede00acb4b18f829965262f76ae83eac
Andreas Volz <andreas@frodo.mittelerde>
2008-08-02 15:38:11 +0200

    removed old glib code from ecore example

--- examples/ecore/dbus_ecore.h
+++ examples/ecore/dbus_ecore.h
@@ -7,17 +7,6 @@
 
 #include "dbus_ecore-glue.h"
 
-/*class DBusInspector
-: public DBus::IntrospectableProxy,
-  public DBus::ObjectProxy
-{
-public:
-
-	DBusInspector( DBus::Connection& conn, const char* path, const char* service )
-	: DBus::ObjectProxy(conn, path, service)
-	{}
-};*/
-
 class DBusBrowser
 : public org::freedesktop::DBus,
   public DBus::IntrospectableProxy,
@@ -35,27 +24,8 @@ private:
 
 	void NameAcquired( const ::DBus::String& );
 
-	/*void on_select_busname();
-
-	void _inspect_append( Gtk::TreeModel::Row*, const std::string&, const std::string& );*/
-
 private:
 
-	/*class InspectRecord : public Gtk::TreeModel::ColumnRecord
-	{
-	public:
-
-		InspectRecord() { add(name); }
-
-		Gtk::TreeModelColumn<Glib::ustring> name;
-	};
-
-	Gtk::VBox			_vbox;
-	Gtk::ScrolledWindow		_sc_tree;
-	Gtk::ComboBoxText		_cb_busnames;
-	Gtk::TreeView			_tv_inspect;
-	Glib::RefPtr<Gtk::TreeStore>	_tm_inspect;*/
-	//InspectRecord			_records;
 };
 
 #endif//__DEMO_DBUS_BROWSER_H
